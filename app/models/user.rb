5ab01457ee6274bd1dd52efbd82cb1067746b7bb
Glenn Gunter <gunterglenn33@gmail.com>
2021-07-05 23:26:01 +0200

    finished controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :event_organizations
+  has_many :bookings, dependent: :destroy
+  has_many :event_organizations, dependent: :destroy
+  has_many :events, through: :event_organizations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

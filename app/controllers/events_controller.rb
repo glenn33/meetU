1f6b8de83503c40c8aaa76fe3c3ee762651a097d
Glenn Gunter <gunterglenn33@gmail.com>
2021-07-05 20:25:06 +0200

    finished controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end

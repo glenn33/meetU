e49ccbc92f9dd13d7d364374dcb5dc140ecea315
Glenn Gunter <gunterglenn33@gmail.com>
2021-07-06 20:38:20 +0200

    finished all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :phone_number, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :phone_number, :profile_picture])
+  end
 end
